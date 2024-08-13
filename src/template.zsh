48b7f885a573dec43904780f203bfc338528592f
const-void <const-void@eml.cc>
2024-08-12 21:51:58 -0400

    feat: @rose-pine/build template for zsh

---
+++ src/template.zsh
@@ -0,0 +1,30 @@
+# $id
+#
+# $name ($type)
+#
+# (C) 2024 const void*, free + open
+
+export TERM16M_THEME="$id"
+
+# Background
+export TERM16M_BASE="$base"    #$id-base
+export TERM16M_SURFACE="$surface"    #$id-surface
+export TERM16M_OVERLAY="$overlay"    #$id-overlay
+
+# Foreground
+export TERM16M_MUTED="$muted"    #$id-muted
+export TERM16M_SUBTLE="$subtle"    #$id-subtle
+export TERM16M_TEXT="$text"    #$id-text
+
+# Accents
+export TERM16M_LOVE="$love"    #$id-love
+export TERM16M_GOLD="$gold"    #$id-gold
+export TERM16M_ROSE="$rose"    #$id-rose
+export TERM16M_PINE="$pine"    #$id-pine
+export TERM16M_FOAM="$foam"    #$id-foam
+export TERM16M_IRIS="$iris"    #$id-iris
+
+# Highlights
+export TERM16M_SEL_L="$highlightLow"    #$id-highlightLow
+export TERM16M_SEL_M="$highlightMed"    #$id-highlightMed
+export TERM16M_SEL_H="$highlightHigh"    #$id-highlightHigh
